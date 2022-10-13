4126c3c1474d3f6cae193ac7ba24be8b2bec3a98
Adama Cissé <adam.cis345@gmail.com>
2022-10-13 21:21:30 +0200

    training

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+noms = ["Jean", "Sophie", "Martin"]
+
+noms_supplementaires = ["Christophe", "Zoe"]
+
+# noms.append(noms_supplementaires)
+# for e in noms_supplementaires:
+#     noms.append(e)
+# noms.extend(noms_supplementaires)
+# noms += noms_supplementaires
+noms = noms_supplementaires + noms
+
+print(noms)
